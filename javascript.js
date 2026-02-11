b948934c602490e77cda0f83f66f4cce4fdfc51b
Barry Banta <46609115+bcbanta@users.noreply.github.com>
2026-02-11 09:26:09 -0600

    added in intro javascript files

---
+++ javascript.js
@@ -0,0 +1,34 @@
+
+let firstName = "John";
+let lastName = "Doe"; 
+
+console.log(firstName);
+console.log(lastName); 
+
+console.log("Math practice:");
+
+console.log(23+97);
+
+console.log((4 + 6 + 9) / 77)
+
+console.log("Variable Practice: \n")
+
+let a =10;
+
+console.log(a);
+
+a = 15;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b)
+
+console.log("Declare Max: \n")
+
+const max = 57;
+const actual = max-13;
+const percentage = actual/max;
+
+console.log(percentage);
